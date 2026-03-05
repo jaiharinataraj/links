8ceb441edbdeeb51f04f4a5ab38f1110bed326f1
ijaihari <sjaiharinataraj@gmail.com>
2026-03-05 22:38:38 +0530

    updated analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import LinksPage from "./pages/LinksPage";
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
   return (
     <>
+      <Analytics />
       <LinksPage />
     </>
   );
